a89bed8f4ee24d665768f615a71ff2856dbb0375
Sabil Firdausi <53112395+firdausi555@users.noreply.github.com>
2025-07-14 22:44:47 +0530

    Create Q_1290.py

---
+++ Q_1290.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head):
+        res=''
+        current= head
+        while current:
+            res+=str(current.val)
+            current=current.next
+        return int(res,2)
+            
